5bd8f3e4eb5916ea6ed428cbd88938a60ace17b3
jerry5585 <jerry5585@yahoo.com>
2021-03-17 10:20:00 +0100

    Nothing changed

--- Webbutik/Program.cs
+++ Webbutik/Program.cs
@@ -7,7 +7,26 @@ namespace Webbutik
     {
         static void Main(string[] args)
         {
+            string username;
+            string password;
+            int loggedIn;
+
             Seeder.Seed();
+            WebbShopAPI webbShopAPI = new WebbShopAPI();
+
+            username = "Administrator";
+            password = "CodicRulez";
+
+            loggedIn = webbShopAPI.Login(username, password);
+
+            if (loggedIn > 0)
+            {
+                Console.WriteLine("Successfully logged in");
+            }
+            else
+            {
+                Console.WriteLine("Wrong username or password");
+            }
         }
     }
 }
